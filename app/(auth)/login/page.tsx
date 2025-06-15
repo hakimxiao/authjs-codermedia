01e434dd5759334624be9c28949db455dc90faa4
abbu solihin alhakim <alhakimabusolihin@gmail.com>
2025-06-15 14:22:10 +0700

    menambahkan suspense

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import FormLogin from '@/components/auth/FormLogin'
 import { GoogleButton, GithubButton } from '@/components/auth/SocialButton'
 import OAuthErrorAlert from '@/components/auth/OAuthAlert';
@@ -8,7 +8,11 @@ const Login = () => {
     return (
         <div className='p-6 space-y-4'>
             <h1 className='text-2xl font-bold text-gray-900'>Sign In to your account</h1>
-            <OAuthErrorAlert />
+
+            <Suspense fallback={null}>
+                <OAuthErrorAlert />
+            </Suspense>
+
             <FormLogin />
             <div className="my-4 flex items-center before:flex-1 before:border-t before:border-gray-300 after:flex-1 after:border-t after:border-gray-300">
                 <p className='mx-4 mb-0 text-center font-semibold text-gray-600'>OR</p>
